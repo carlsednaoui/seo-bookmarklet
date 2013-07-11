bd1416194f7bc69415a78d8784883fc80c6d80f5
carl sednaoui <carlsednaoui@gmail.com>
2013-07-11 16:25:16 -0400

    Check jQuery version and update jQuery if needed

--- ouiseo.js
+++ ouiseo.js
@@ -1,15 +1,30 @@
-// Need to deal with HTTPS
-if (!($ = window.jQuery)) {  // load jQuery if not present
-  script        = document.createElement( 'script' );
-  script.src    = 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js';
-  script.onload = ouiseo;
+(function() {
+  // Need to deal with HTTPS
 
-  document.body.appendChild(script);
-  console.log('ouiseo loaded jQuery');
-} else {
-  ouiseo();
-}
+  // The minimum version of jQuery we want
+  var v = '1.10.2';
 
-function ouiseo() {
-  console.log('ouiseo has been loaded');
-}
+  // Check if jQuery is present and jQuery version
+  if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+    var done      = false,
+    script        = document.createElement('script');
+    script.src    = 'http://ajax.googleapis.com/ajax/libs/jquery/' + v + '/jquery.min.js';
+
+    script.onload = script.onreadystatechange = function() {
+      if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+        done = true;
+        initOuiseo();
+      }
+    };
+    console.log('ouiseo loaded jQuery');
+    document.getElementsByTagName('head')[0].appendChild(script);
+  } else {
+    initOuiseo();
+  }
+
+  function initOuiseo() {
+    (window.ouiseo = function() {
+      console.log('ouiseo has been loaded');
+    })();
+  }
+})();
