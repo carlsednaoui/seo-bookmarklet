1a719d3a8925e426b4c0960a90c75ef2d87606e0
carl sednaoui <carlsednaoui@gmail.com>
2013-07-15 16:35:07 -0400

    Remove dud files

--- ouiseo.js
+++ ouiseo.js
@@ -25,26 +25,30 @@
   function initOuiseo() {
     (window.ouiseo = function() {
       $("body").append("\
-        <div id='wikiframe'>\
-          <div id='wikiframe_veil' style=''>\
-            <p>Loading...</p>\
+        <div id='ouiseo_frame'>\
+        </div>\
+        <div id='ouiseo' style=''>\
+          <div id='ouiseo-basic-seo'>\
+            <p>Title: <input type='text' id='ouiseo-title' class='ouiseo-input-text'></p>\
+            <p>Meta Description: <input type='text' id='ouiseo-description' class='ouiseo-input-text'></p>\
           </div>\
-          <iframe src='http://en.wikipedia.org/w/index.php?&search="+'banana'+"' onload=\"$('#wikiframe iframe').slideDown(500);\">Enable iFrames.</iframe>\
+        </div>\
           <style type='text/css'>\
-            #wikiframe_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }\
-            #wikiframe_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }\
-            #wikiframe iframe { display: none; position: fixed; top: 10%; left: 10%; width: 80%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }\
-          </style>\
-        </div>");
-        $("#wikiframe_veil").fadeIn(750);
+            #ouiseo_frame { position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }\
+            #ouiseo { display: none; position: fixed; top: 10%; left: 10%; width: 80%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }\
+            .ouiseo-input-text { width: 300px;}\
+          </style>");
+        $("#ouiseo").fadeIn(750);
 
-        $("#wikiframe_veil").click(function(event){
-          $("#wikiframe_veil").fadeOut(750);
-          $("#wikiframe iframe").slideUp(500);
-          setTimeout("$('#wikiframe').remove()", 750);
+        $("#ouiseo_frame").click(function(event){
+          $("#ouiseo").fadeOut(750);
+          $("#ouiseo_frame").slideUp(500);
+          setTimeout("$('#ouiseo_frame').remove()", 750);
       });
+      $('#ouiseo-title').val($('title').text());
+      $('#ouiseo-description').val($('meta[name=description]').attr('content'));
     })();
   }
 })();
 
-// <a href="javascript:(function(){if(window.myBookmarklet!==undefined){myBookmarklet();}else{document.body.appendChild(document.createElement('script')).src='http://iamnotagoodartist.com/stuff/wikiframe2.js?';}})();">WikiFrame</a>
+// <a href="javascript:(function(){if(window.myBookmarklet!==undefined){myBookmarklet();}else{document.body.appendChild(document.createElement('script')).src='http://iamnotagoodartist.com/stuff/ouiseoframe2.js?';}})();">WikiFrame</a>
